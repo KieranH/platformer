e56b708e2afca52fcbe3f14ae5eb44b0bcd84034
KieranH <ford121@gmail.com>
2014-03-06 10:00:35 +0000

    started work adding selection to graphic menu

--- menu-test.py
+++ menu-test.py
@@ -6,6 +6,9 @@ DISPLAY = (800, 640)
 DEPTH = 32
 FLAGS = 0
 
+#will be used for an array
+current_position = 0
+
 pygame.init()
 
 #define font that will be used throughout the game
@@ -19,7 +22,15 @@ def render_text(text, colour):
 	
 for item in text:
 	item[2][0] = render_text(item[0], item[1][0])
-	item[2][1] = render_text(item[0], (255, 0, 0))
+	item[2][1] = render_text(">"+item[0]+"<", (255, 0, 0))
+
+def move_pos(num, list):
+	global current_position
+	if current_position + num < len(list) and current_position + num >= 0:
+		current_position = current_position + num
+	elif current_position + num == len(list):
+		current_position = 0
+	else: current_position = len(list) - 1
 
 def menu():
 	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
@@ -29,7 +40,7 @@ def menu():
 	
 	while True:
 		#limit framerate to 60fps
-		clock.tick(1000000000)
+		clock.tick(60)
 		pygame.display.set_caption("MENU TEST - FPS = %.2f" % (clock.get_fps()))
 		
 		#Events
@@ -39,9 +50,17 @@ def menu():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
 					sys.exit()
+				if event.key == pygame.K_UP:
+					move_pos(-1, text)
+				if event.key == pygame.K_DOWN:
+					move_pos(1, text)
 		for item in text:
-			screen.blit(item[2][0], item[1][1])
+			if item == text[current_position]:
+				screen.blit(item[2][1], item[1][1])
+			else:
+				screen.blit(item[2][0], item[1][1])
 	
 		pygame.display.flip()
+
 		
 if __name__ == "__main__": menu()
