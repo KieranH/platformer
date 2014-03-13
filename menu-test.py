7dd0fdfb4ba76f60479093ca4c0f9e3945d6e7f3
KieranH <ford121@gmail.com>
2014-03-13 09:33:09 +0000

    added functionality for selection in menu
    The menu calls a function to see what is selected and lets the game move
    to the next step (play, instructions)
    
    System will also gracefully crash if a position out of bounds is
    selected for some reason

--- menu-test.py
+++ menu-test.py
@@ -14,7 +14,8 @@ pygame.init()
 #define font that will be used throughout the game
 #print pygame.font.get_fonts()
 font = pygame.font.SysFont("arial", 20)
-print font
+
+#setup array to hold text data
 text = [["Play", [(0,0,0), (100,100)], [None, None]], ["Instructions", [(0,0,0), (100,200)], [None, None]]]
 
 def render_text(text, colour):
@@ -31,6 +32,15 @@ def move_pos(num, list):
 	elif current_position + num == len(list):
 		current_position = 0
 	else: current_position = len(list) - 1
+	
+def next_event(pos):
+	if pos == 0:
+		#main()
+		print "game loop"
+	elif pos == 1:
+		print "instructions"
+	else:
+		sys.exit()
 
 def menu():
 	screen = pygame.display.set_mode(DISPLAY, FLAGS, DEPTH)
@@ -41,26 +51,34 @@ def menu():
 	while True:
 		#limit framerate to 60fps
 		clock.tick(60)
+		#set title
 		pygame.display.set_caption("MENU TEST - FPS = %.2f" % (clock.get_fps()))
 		
 		#Events
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
+				#exit cleanly
 				sys.exit()
 			if event.type == pygame.KEYDOWN:
+				#Handle keyboard inputs
 				if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
 					sys.exit()
 				if event.key == pygame.K_UP:
 					move_pos(-1, text)
 				if event.key == pygame.K_DOWN:
 					move_pos(1, text)
+				if event.key == pygame.K_RETURN:
+					next_event(current_position)
 		for item in text:
+			#if the current item is the same as the user selected one, draw it in a different colour
 			if item == text[current_position]:
 				screen.blit(item[2][1], item[1][1])
 			else:
 				screen.blit(item[2][0], item[1][1])
 	
+		#draw changes to the screen
 		pygame.display.flip()
-
+		#fill in the screen so we can write text over it again
+		screen.fill((255,255,255))
 		
 if __name__ == "__main__": menu()
